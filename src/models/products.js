e3be961e947594aa73dbd75e15e7b7b368dbefd3
rodlumon35 <rodlumon35@gmail.com>
2021-08-25 15:36:17 +0100

    product model modifications

--- src/models/products.js
+++ src/models/products.js
@@ -1,13 +1,19 @@
 import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
-const productSchema = new Schema({
-  name: { type: String, default: "undefined" },
-  category: [{ type: String, default: "undefined" }],
-  price: { type: Number, default: 0 },
-  quantity: { type: Number, default: 0 },
-  image: { type: Buffer, contentType: String, default: "undefined" },
-  description: { type: String, default: "undefined" },
-});
+const productSchema = new Schema(
+  {
+    name: { type: String, default: "undefined" },
+    category: [{ type: String, default: "undefined" }],
+    price: { type: Number, default: 0 },
+    quantity: { type: Number, default: 0 },
+    image: { type: Buffer, contentType: String, default: "undefined" },
+    description: { type: String, default: "undefined" },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
 export default model("Product", productSchema);
